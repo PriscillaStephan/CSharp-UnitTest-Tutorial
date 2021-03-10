c09d0ce6f68788982451f99132f9dd0b7f17fb6c
SAMER\SAMER <samer@onsite-mena.com>
2021-03-10 18:47:55 +0200

    First commit

---
+++ Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSTestingCourse.Fundamentals
+{
+    public class HtmlFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
---
+++ Fundamentals/Reservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSTestingCourse.FIndamentals
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+
+            //before refactoring 
+            if (user.IsAdmin)
+                return true;
+
+            if (MadeBy == user)
+                return true;
+
+            return false;
+
+            //after refactoring
+            //return (user.IsAdmin || MadeBy == user);
+        }
+    
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
